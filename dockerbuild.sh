7ffbb57708afe680384409ae4b76ab567950ddb1
todor.ivanov <todor.ivanov.work@gmail.com>
2021-11-01 10:12:15 +0200

    Added a shell script for building and pushing the Docker image

---
+++ dockerbuild.sh
@@ -0,0 +1,9 @@
+echo "Build version: "
+read buildVersion
+repoName = "wpmt-cluster-log"
+buildCommand = "docker build -t dev/$repoName:$buildVersion -f Dockerfile ."
+tagCommand = "docker tag dev/$repoName:$buildVersion docker-registry.wpmt.org/docker-user/$repoName:$buildVersion"
+pushCommand = "docker push docker-registry.wpmt.org/docker-user/$repoName:$buildVersion"
+eval "$buildCommand"
+eval "$tagCommand"
+eval "$pushCommand"
